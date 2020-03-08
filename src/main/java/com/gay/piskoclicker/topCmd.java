a29877934463d15f19d76084731df4549e74a8f2
BuildTools <unconfigured@null.spigotmc.org>
2020-03-09 00:45:01 +0300

    IN PROCESS

--- src/main/java/com/gay/piskoclicker/topCmd.java
+++ src/main/java/com/gay/piskoclicker/topCmd.java
@@ -16,55 +16,56 @@ public class topCmd implements CommandExecutor {
     public boolean onCommand(CommandSender sender, Command cmd, String label, String[] args) {
         if (sender instanceof Player) {
             Player player = (Player) sender;
-            HashSet<UUID> uuids = new HashSet<>();
-            for (OfflinePlayer player1 : Bukkit.getServer().getOfflinePlayers()) uuids.add(player1.getUniqueId());
-            for (Player player2 : Bukkit.getServer().getOnlinePlayers()) uuids.add(player2.getUniqueId());
-            UUID[] realUUIDs = new UUID[uuids.size()];
-            UUID inUUID;
-            for (int i = 0; i < uuids.size(); i++) {
-                for (int j = i + 1; j < uuids.size(); j++) {
-                    if (Events.getBalanceDoubleUUID(realUUIDs[i]) < Events.getBalanceDoubleUUID(realUUIDs[j])) {
-                        inUUID = realUUIDs[i];
-                        realUUIDs[i] = realUUIDs[j];
-                        realUUIDs[j] = inUUID;
-                    }
-                }
-            }
-            StringBuilder builder = new StringBuilder();
-            for (int i = 0; i < 10; i++) {
-                if (i > realUUIDs.length) {
-                    player.sendMessage(builder.toString());
-                    return true;
-                }
-                switch (i) {
-                    case (0):
-                        if (Bukkit.getServer().getOnlinePlayers().contains(Bukkit.getServer().getPlayer(realUUIDs[i])))
-                            builder.append(" ").append(ChatColor.RED).append(ChatColor.BOLD).append(i + 1).append(". ").append(ChatColor.YELLOW).append(Bukkit.getServer().getPlayer(realUUIDs[i]).getName()).append(ChatColor.RESET).append(ChatColor.GOLD).append(" [").append(ChatColor.LIGHT_PURPLE).append((int) Events.getBalanceDoubleUUID(realUUIDs[i])).append(ChatColor.GOLD).append("]").append(ChatColor.RESET).append("\n");
-                        else
-                            System.out.println(realUUIDs[i]);
-                            builder.append(" ").append(ChatColor.RED).append(ChatColor.BOLD).append(i + 1).append(". ").append(ChatColor.YELLOW).append(Bukkit.getServer().getOfflinePlayer(realUUIDs[i]).getName()).append(ChatColor.RESET).append(ChatColor.GOLD).append(" [").append(ChatColor.LIGHT_PURPLE).append((int) Events.getBalanceDoubleUUID(realUUIDs[i])).append(ChatColor.GOLD).append("]").append(ChatColor.RESET).append("\n");
-                        break;
-                    case (1):
-                        if (Bukkit.getServer().getOnlinePlayers().contains(Bukkit.getServer().getPlayer(realUUIDs[i])))
-                            builder.append(" ").append(ChatColor.RED).append(ChatColor.BOLD).append(i + 1).append(". ").append(ChatColor.WHITE).append(Bukkit.getServer().getPlayer(realUUIDs[i]).getName()).append(ChatColor.RESET).append(ChatColor.GOLD).append(" [").append(ChatColor.LIGHT_PURPLE).append((int) Events.getBalanceDoubleUUID(realUUIDs[i])).append(ChatColor.GOLD).append("]").append(ChatColor.RESET).append("\n");
-                        else
-                            builder.append(" ").append(ChatColor.RED).append(ChatColor.BOLD).append(i + 1).append(". ").append(ChatColor.WHITE).append(Bukkit.getServer().getOfflinePlayer(realUUIDs[i]).getName()).append(ChatColor.RESET).append(ChatColor.GOLD).append(" [").append(ChatColor.LIGHT_PURPLE).append((int) Events.getBalanceDoubleUUID(realUUIDs[i])).append(ChatColor.GOLD).append("]").append(ChatColor.RESET).append("\n");
-                        break;
-                    case (2):
-                        if (Bukkit.getServer().getOnlinePlayers().contains(Bukkit.getServer().getPlayer(realUUIDs[i])))
-                            builder.append(" ").append(ChatColor.RED).append(ChatColor.BOLD).append(i + 1).append(". ").append(ChatColor.GOLD).append(Bukkit.getServer().getPlayer(realUUIDs[i]).getName()).append(ChatColor.RESET).append(ChatColor.GOLD).append(" [").append(ChatColor.LIGHT_PURPLE).append((int) Events.getBalanceDoubleUUID(realUUIDs[i])).append(ChatColor.GOLD).append("]").append(ChatColor.RESET).append("\n");
-                        else
-                            builder.append(" ").append(ChatColor.RED).append(ChatColor.BOLD).append(i + 1).append(". ").append(ChatColor.GOLD).append(Bukkit.getServer().getOfflinePlayer(realUUIDs[i]).getName()).append(ChatColor.RESET).append(ChatColor.GOLD).append(" [").append(ChatColor.LIGHT_PURPLE).append((int) Events.getBalanceDoubleUUID(realUUIDs[i])).append(ChatColor.GOLD).append("]").append(ChatColor.RESET).append("\n");
-                        break;
-                    default:
-                        if (Bukkit.getServer().getOnlinePlayers().contains(Bukkit.getServer().getPlayer(realUUIDs[i])))
-                            builder.append(" ").append(ChatColor.RED).append(ChatColor.BOLD).append(i + 1).append(". ").append(ChatColor.GRAY).append(Bukkit.getServer().getPlayer(realUUIDs[i]).getName()).append(ChatColor.RESET).append(ChatColor.GOLD).append(" [").append(ChatColor.LIGHT_PURPLE).append((int) Events.getBalanceDoubleUUID(realUUIDs[i])).append(ChatColor.GOLD).append("]").append(ChatColor.RESET).append("\n");
-                        else
-                            builder.append(" ").append(ChatColor.RED).append(ChatColor.BOLD).append(i + 1).append(". ").append(ChatColor.GRAY).append(Bukkit.getServer().getOfflinePlayer(realUUIDs[i]).getName()).append(ChatColor.RESET).append(ChatColor.GOLD).append(" [").append(ChatColor.LIGHT_PURPLE).append((int) Events.getBalanceDoubleUUID(realUUIDs[i])).append(ChatColor.GOLD).append("]").append(ChatColor.RESET).append("\n");
-                        break;
-                }
-            }
-            player.sendMessage(builder.toString());
+            player.sendMessage(ChatColor.DARK_AQUA + "В разработке...");
+//            HashSet<UUID> uuids = new HashSet<>();
+//            for (OfflinePlayer player1 : Bukkit.getServer().getOfflinePlayers()) uuids.add(player1.getUniqueId());
+//            for (Player player2 : Bukkit.getServer().getOnlinePlayers()) uuids.add(player2.getUniqueId());
+//            UUID[] realUUIDs = new UUID[uuids.size()];
+//            UUID inUUID;
+//            for (int i = 0; i < uuids.size(); i++) {
+//                for (int j = i + 1; j < uuids.size(); j++) {
+//                    if (Events.getBalanceDoubleUUID(realUUIDs[i]) < Events.getBalanceDoubleUUID(realUUIDs[j])) {
+//                        inUUID = realUUIDs[i];
+//                        realUUIDs[i] = realUUIDs[j];
+//                        realUUIDs[j] = inUUID;
+//                    }
+//                }
+//            }
+//            StringBuilder builder = new StringBuilder();
+//            for (int i = 0; i < 10; i++) {
+//                if (i > realUUIDs.length) {
+//                    player.sendMessage(builder.toString());
+//                    return true;
+//                }
+//                switch (i) {
+//                    case 0:
+//                        if (Bukkit.getServer().getOnlinePlayers().contains(Bukkit.getServer().getPlayer(realUUIDs[i])))
+//                            builder.append(" ").append(ChatColor.RED).append(ChatColor.BOLD).append(i + 1).append(". ").append(ChatColor.YELLOW).append(Bukkit.getServer().getPlayer(realUUIDs[i]).getName()).append(ChatColor.RESET).append(ChatColor.GOLD).append(" [").append(ChatColor.LIGHT_PURPLE).append((int) Events.getBalanceDoubleUUID(realUUIDs[i])).append(ChatColor.GOLD).append("]").append(ChatColor.RESET).append("\n");
+//                        else
+//                            System.out.println(realUUIDs[i]);
+//                            builder.append(" ").append(ChatColor.RED).append(ChatColor.BOLD).append(i + 1).append(". ").append(ChatColor.YELLOW).append(Bukkit.getServer().getOfflinePlayer(realUUIDs[i]).getName()).append(ChatColor.RESET).append(ChatColor.GOLD).append(" [").append(ChatColor.LIGHT_PURPLE).append((int) Events.getBalanceDoubleUUID(realUUIDs[i])).append(ChatColor.GOLD).append("]").append(ChatColor.RESET).append("\n");
+//                        break;
+//                    case 1:
+//                        if (Bukkit.getServer().getOnlinePlayers().contains(Bukkit.getServer().getPlayer(realUUIDs[i])))
+//                            builder.append(" ").append(ChatColor.RED).append(ChatColor.BOLD).append(i + 1).append(". ").append(ChatColor.WHITE).append(Bukkit.getServer().getPlayer(realUUIDs[i]).getName()).append(ChatColor.RESET).append(ChatColor.GOLD).append(" [").append(ChatColor.LIGHT_PURPLE).append((int) Events.getBalanceDoubleUUID(realUUIDs[i])).append(ChatColor.GOLD).append("]").append(ChatColor.RESET).append("\n");
+//                        else
+//                            builder.append(" ").append(ChatColor.RED).append(ChatColor.BOLD).append(i + 1).append(". ").append(ChatColor.WHITE).append(Bukkit.getServer().getOfflinePlayer(realUUIDs[i]).getName()).append(ChatColor.RESET).append(ChatColor.GOLD).append(" [").append(ChatColor.LIGHT_PURPLE).append((int) Events.getBalanceDoubleUUID(realUUIDs[i])).append(ChatColor.GOLD).append("]").append(ChatColor.RESET).append("\n");
+//                        break;
+//                    case 2:
+//                        if (Bukkit.getServer().getOnlinePlayers().contains(Bukkit.getServer().getPlayer(realUUIDs[i])))
+//                            builder.append(" ").append(ChatColor.RED).append(ChatColor.BOLD).append(i + 1).append(". ").append(ChatColor.GOLD).append(Bukkit.getServer().getPlayer(realUUIDs[i]).getName()).append(ChatColor.RESET).append(ChatColor.GOLD).append(" [").append(ChatColor.LIGHT_PURPLE).append((int) Events.getBalanceDoubleUUID(realUUIDs[i])).append(ChatColor.GOLD).append("]").append(ChatColor.RESET).append("\n");
+//                        else
+//                            builder.append(" ").append(ChatColor.RED).append(ChatColor.BOLD).append(i + 1).append(". ").append(ChatColor.GOLD).append(Bukkit.getServer().getOfflinePlayer(realUUIDs[i]).getName()).append(ChatColor.RESET).append(ChatColor.GOLD).append(" [").append(ChatColor.LIGHT_PURPLE).append((int) Events.getBalanceDoubleUUID(realUUIDs[i])).append(ChatColor.GOLD).append("]").append(ChatColor.RESET).append("\n");
+//                        break;
+//                    default:
+//                        if (Bukkit.getServer().getOnlinePlayers().contains(Bukkit.getServer().getPlayer(realUUIDs[i])))
+//                            builder.append(" ").append(ChatColor.RED).append(ChatColor.BOLD).append(i + 1).append(". ").append(ChatColor.GRAY).append(Bukkit.getServer().getPlayer(realUUIDs[i]).getName()).append(ChatColor.RESET).append(ChatColor.GOLD).append(" [").append(ChatColor.LIGHT_PURPLE).append((int) Events.getBalanceDoubleUUID(realUUIDs[i])).append(ChatColor.GOLD).append("]").append(ChatColor.RESET).append("\n");
+//                        else
+//                            builder.append(" ").append(ChatColor.RED).append(ChatColor.BOLD).append(i + 1).append(". ").append(ChatColor.GRAY).append(Bukkit.getServer().getOfflinePlayer(realUUIDs[i]).getName()).append(ChatColor.RESET).append(ChatColor.GOLD).append(" [").append(ChatColor.LIGHT_PURPLE).append((int) Events.getBalanceDoubleUUID(realUUIDs[i])).append(ChatColor.GOLD).append("]").append(ChatColor.RESET).append("\n");
+//                        break;
+//                }
+//            }
+//            player.sendMessage(builder.toString());
         }
         return true;
     }
